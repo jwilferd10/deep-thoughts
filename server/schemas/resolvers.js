29b826e9feab57b8920f8ad127101126cd9134c7
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-17 20:29:40 -0700

    Feat: Finished setting up the schemas and downloading the required packages

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,31 @@
+const { User, Thought } = require('../models');
+
+const resolvers = {
+    Query: {
+        // Get all thoughts
+        thoughts: async () => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+        // Get a thought by id
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+        // Get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // Get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+    }
+};
+  
+module.exports = resolvers;
